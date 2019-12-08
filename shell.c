e028b63d97967ce531fe95cdf16c65900b89c330
mohamedelraghy <elraghy8@gmail.com>
2019-12-09 01:33:50 +0200

    init the shell

--- shell.c
+++ shell.c
@@ -1,6 +1,24 @@
 #include<stdio.h>
 
+#define clear() printf("\033[H\033[J")
+
+void init_shell();
 
 int main(){
 
+}
+
+void init_shell(){
+    clear();
+    printf("\n\n\n\n******************"
+           "************************");
+    printf("\n\n\n\t****MY SHELL****");
+    printf("\n\n\t-USE AT YOUR OWN RISK-");
+    printf("\n\n\n\n*******************"
+           "***********************");
+    char *username = getenv("USER");
+    printf("\n\n\nUSER is: @%s", username);
+    printf("\n");
+    sleep(1);
+    clear();
 }
