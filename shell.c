a6013fef1d86eac19d1baf68618ff73655a56936
mohamedelraghy <elraghy8@gmail.com>
2019-12-09 01:47:52 +0200

    function to take input

--- shell.c
+++ shell.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
+#include<readline/readline.h>
+#include<readline/history.h>  
+#include<string.h>
 
 #define clear() printf("\033[H\033[J")
 
 void init_shell();
-
+int takeInput(char * );
 int main(){
 
 }
@@ -23,4 +26,14 @@ void init_shell(){
     printf("\n");
     sleep(1);
     clear();
+}
+
+int takeInput(char *str){
+    char *buf;
+    buf = readline("\n>>> ");
+    if(strlen(buf) != 0) {
+        add_history(buf);
+        strcpy(str, buf);
+        return 0;
+    } else return 1;
 }
