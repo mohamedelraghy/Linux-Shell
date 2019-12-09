e0df2f2cb0517d7a12678e4360722ea56d96c751
mohamedelraghy <elraghy8@gmail.com>
2019-12-09 02:39:02 +0200

    function to exec bulitin commmands

--- shell.c
+++ shell.c
@@ -15,8 +15,9 @@ void pwd();
 void execArgs(char **);
 void execArgsPiped(char **, char **);
 void openHelp();
+int ownCmdHandler(char **);
 
-int main()
+    int main()
 {
 }
 
@@ -135,3 +136,46 @@ void openHelp(){
     return;
 }
 
+int ownCmdHandler(char **parsed) {
+    int NoOfOwnCmds = 4, i, switchOwnArg = 0;
+    char *ListOfOwnCmds[NoOfOwnCmds];
+    char *username;
+
+    ListOfOwnCmds[0] = "exit";
+    ListOfOwnCmds[1] = "cd";
+    ListOfOwnCmds[2] = "help";
+    ListOfOwnCmds[3] = "hello";
+
+    for (i = 0; i < NoOfOwnCmds; i++)
+    {
+        if (strcmp(parsed[0], ListOfOwnCmds[i]) == 0)
+        {
+            switchOwnArg = i + 1;
+            break;
+        }
+    }
+
+    switch (switchOwnArg)
+    {
+    case 1:
+        printf("\nGoodbye\n");
+        exit(0);
+    case 2:
+        chdir(parsed[1]);
+        return 1;
+    case 3:
+        openHelp();
+        return 1;
+    case 4:
+        username = getenv("USER");
+        printf("\nHello %s.\nMind that this is "
+               "not a place to play around."
+               "\nUse help to know more..\n",
+               username);
+        return 1;
+    default:
+        break;
+    }
+
+    return 0;
+}
