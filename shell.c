47e479a75babd9dadf3536edae496cfa8f2a4abd
mohamedelraghy <elraghy8@gmail.com>
2019-12-09 02:37:42 +0200

    help function 🤗️

--- shell.c
+++ shell.c
@@ -14,8 +14,10 @@ int takeInput(char * );
 void pwd();
 void execArgs(char **);
 void execArgsPiped(char **, char **);
+void openHelp();
 
-int main(){
+int main()
+{
 }
 
 void init_shell(){
@@ -117,3 +119,19 @@ void execArgsPiped(char **parsed, char **parsedpipe){
         }
     }
 }
+
+void openHelp(){
+    puts("\n***WELCOME TO MY SHELL HELP***"
+         "\nCopyright @ Suprotik Dey"
+         "\n-Use the shell at your own risk..."
+         "\nList of Commands supported:"
+         "\n>cd"
+         "\n>ls"
+         "\n>exit"
+         "\n>all other general commands available in UNIX shell"
+         "\n>pipe handling"
+         "\n>improper space handling");
+
+    return;
+}
+
