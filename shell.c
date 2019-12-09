fad08a77b5da943dd1e9288de16bf1baa5fdc1f7
mohamedelraghy <elraghy8@gmail.com>
2019-12-09 02:45:54 +0200

    Main Entry point to the program

--- shell.c
+++ shell.c
@@ -21,8 +21,36 @@ int ownCmdHandler(char **);
 int parsePipe(char *, char **);
 void parseSpace(char *, char **);
 int processString(char *str, char **parsed, char **parsedpipe);
-    int main()
-{
+
+int main() {
+    char inputString[MAXCOM], *parsedArgs[MAXLIST];
+    char *parsedArgsPiped[MAXLIST];
+    int execFlag = 0;
+    init_shell();
+
+    while (1)
+    {
+        // print shell line
+        printDir();
+        // take input
+        if (takeInput(inputString))
+            continue;
+        // process
+        execFlag = processString(inputString,
+                                 parsedArgs, parsedArgsPiped);
+        // execflag returns zero if there is no command
+        // or it is a builtin command,
+        // 1 if it is a simple command
+        // 2 if it is including a pipe.
+
+        // execute
+        if (execFlag == 1)
+            execArgs(parsedArgs);
+
+        if (execFlag == 2)
+            execArgsPiped(parsedArgs, parsedArgsPiped);
+    }
+    return 0;
 }
 
 void init_shell(){
