6ea7228b8be65c8671f68c36670464336240e902
mohamedelraghy <elraghy8@gmail.com>
2019-12-10 17:57:32 +0200

    use mutex for achieving thread synchronization

--- thread.c
+++ thread.c
@@ -3,10 +3,14 @@
 #define NUM_THREADS 5
 
 int sum = 0;
-
+pthread_mutex_t lock;
 
 void *run (void * args){
+    pthread_mutex_lock(&lock);
+
     for(int i = 0; i < 10000000; i++) sum += 1;
+
+    pthread_mutex_unlock(&lock);
     return NULL;
 }
 
@@ -15,12 +19,11 @@ int main (){
 
     for(int i = 0; i < NUM_THREADS; i++) pthread_create(&thread_id[i], NULL, run, NULL);
 
-    void * thread_return[NUM_THREADS];
+    for(int i = 0; i < NUM_THREADS; i++) pthread_join(thread_id[i], NULL);
 
-    for(int i = 0; i < NUM_THREADS; i++) pthread_join(thread_id[i], &thread_return[i]);
-
-    printf("Sum %d", sum);
+    printf("Sum %d\n", sum);
     printf("End of the Program\n");
+    pthread_mutex_destroy(&lock);
 
     return 0;
 }
